082689c65e0229fd25bcde2d4739c903a97fa9a6
lyz <56433542+20191844301@users.noreply.github.com>
2026-04-06 15:36:11 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.optim import Adam
-from torch.optim.lr_scheduler import CyclicLR
 from torch_geometric.nn import (
     GCNConv, GraphConv, TAGConv, GATConv,
     HypergraphConv, ClusterGCNConv, SimpleConv,
@@ -11,6 +9,7 @@ from torch_geometric.nn import (
     TransformerConv, SSGConv, DNAConv, FAConv, LGConv,
     GCN2Conv
 )
+
 
 class ConvPool(nn.Module):
     """
@@ -21,9 +20,9 @@ class ConvPool(nn.Module):
         super().__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
-        self.convs = nn.ModuleDict()  # use ModuleDict to register submodules
+        self.convs = nn.ModuleDict()
 
-        # homogeneous convs (single in_dim)
+        # homogeneous convs
         hom_convs = {
             'GCNConv': GCNConv,
             'TAGConv': TAGConv,
@@ -41,7 +40,8 @@ class ConvPool(nn.Module):
             'LGConv': LGConv,
             'GCN2Conv': GCN2Conv,
         }
-        # hetero convs (may expect tuple in/out sizes)
+
+        # hetero convs
         hetero_convs = {
             'GraphConv': GraphConv,
             'LEConv': LEConv,
@@ -54,30 +54,25 @@ class ConvPool(nn.Module):
 
         all_convs = {**hom_convs, **hetero_convs}
         for name, ConvCls in all_convs.items():
-            # Try a couple of constructor signatures to be robust
             try:
-                # attempt common signature (in_dim, out_dim)
                 self.convs[name] = ConvCls(in_dim, out_dim)
             except Exception:
                 try:
-                    # if in_dim is tuple (in_src, in_dst), try to expand
                     if isinstance(in_dim, (list, tuple)) and len(in_dim) == 2:
                         self.convs[name] = ConvCls(in_dim[0], in_dim[1], out_dim)
                     else:
-                        # try passing as separate args if ConvCls expects two numbers
                         if isinstance(in_dim, (list, tuple)):
                             self.convs[name] = ConvCls(in_dim, out_dim)
                         else:
-                            # final fallback: try no-arg construction (some convs have different APIs)
                             self.convs[name] = ConvCls(out_dim)
                 except Exception:
-                    # skip convs that are incompatible on this environment / version
                     continue
 
     def get_conv(self, name: str) -> nn.Module:
         if name not in self.convs:
             raise KeyError(f"Conv '{name}' not supported. Available: {list(self.convs.keys())}")
         return self.convs[name]
+
 
 class DrugRepositioningModel(nn.Module):
     def __init__(self, config):
@@ -86,23 +81,19 @@ class DrugRepositioningModel(nn.Module):
         self.d_emb = nn.Embedding(config['disease_size'], D)
         self.p_emb = nn.Embedding(config['drug_size'], P)
 
-        # config for conv types
         het_conv = config.get('het_conv', 'GraphConv')
         hom_conv = config.get('hom_conv', 'GCNConv')
 
-        # conv pools
         pool_dp = ConvPool((D, P), L)
         pool_pd = ConvPool((P, D), L)
         pool_dd = ConvPool(D, L)
         pool_pp = ConvPool(P, L)
 
-        # assign conv layers
         self.conv_dp = pool_dp.get_conv(het_conv)
         self.conv_pd = pool_pd.get_conv(het_conv)
         self.conv_dd = pool_dd.get_conv(hom_conv)
         self.conv_pp = pool_pp.get_conv(hom_conv)
 
-        # mask networks for homogeneous edge weights
         self.mask_dd = nn.Sequential(
             nn.Linear(2 * D, L), nn.ReLU(), nn.Linear(L, 1)
         )
@@ -110,16 +101,15 @@ class DrugRepositioningModel(nn.Module):
             nn.Linear(2 * P, L), nn.ReLU(), nn.Linear(L, 1)
         )
 
-        # temperature
         self.temp = nn.Parameter(torch.tensor(config['init_temp']), requires_grad=False)
         self.max_temp = config['max_temp']
         self.temp_anneal = config['temp_anneal']
 
-        # downstream MLP & predictor with SELU
         mlp_het, mlp_hom = [], []
         for _ in range(config.get('mlp_layer_num', 1)):
             mlp_het += [nn.Linear(L, L), nn.SELU()]
             mlp_hom += [nn.Linear(L, L), nn.SELU()]
+
         self.mlp_het = nn.Sequential(*mlp_het)
         self.mlp_hom = nn.Sequential(*mlp_hom)
         self.project = nn.Sequential(nn.Linear(2 * L, L), nn.SELU())
@@ -132,6 +122,7 @@ class DrugRepositioningModel(nn.Module):
         # Hetero DP
         out_dp = self.conv_dp((x_d, x_p), e_dp)
         h_dp = F.selu(out_dp)
+
         # Hetero PD
         out_pd = self.conv_pd((x_p, x_d), e_pd)
         h_pd = F.selu(out_pd)
@@ -155,12 +146,10 @@ class DrugRepositioningModel(nn.Module):
         # Gather per-sample embeddings
         z_homo = h_dd[d_idx] * h_pp[p_idx]
         z_het = h_dp[p_idx] * h_pd[d_idx]
-        # Apply MLPs
+
         z2_h = self.mlp_hom(z_homo)
         z2_t = self.mlp_het(z_het)
 
-        # Concatenate and predict
-        
         z_cat = torch.cat([z2_t, z2_h], dim=1)
         z_fuse = self.project(z_cat)
         logits = self.pred(z_fuse).squeeze(-1)
@@ -168,51 +157,6 @@ class DrugRepositioningModel(nn.Module):
 
         fuse_loss = F.mse_loss(z2_t, z2_h)
         return out, logits, fuse_loss, weights
-
-    
-    
-    def configure_optimizers(self, config):
-
-        hetero_params = []
-        other_decay_params = []
-        no_decay_params = []
-
-        for name, param in self.named_parameters():
-            if not param.requires_grad:
-                continue
-            if ('mask_dd' in name) or ('mask_pp' in name) or name.endswith('.bias') or ('temp' in name):
-                no_decay_params.append(param)
-            elif ('conv_dp' in name) or ('conv_pd' in name):
-                hetero_params.append(param)
-            else:
-                other_decay_params.append(param)
-
-        param_groups = []
-        het_wd = config.get('weight_decay_het', 0.01)
-        other_wd = config.get('weight_decay', 0.0001)
-
-        if len(hetero_params) > 0:
-            param_groups.append({'params': hetero_params, 'weight_decay': het_wd})
-        if len(other_decay_params) > 0:
-            param_groups.append({'params': other_decay_params, 'weight_decay': other_wd})
-        if len(no_decay_params) > 0:
-            param_groups.append({'params': no_decay_params, 'weight_decay': 0.0001})
-
-        optim = torch.optim.Adam(param_groups, lr=config['lr'])
-
-        sched = None
-        if config.get('use_cyclic', False):
-            sched = CyclicLR(
-                optim,
-                base_lr=config['lr'] * config.get('lr_scale'),
-                max_lr=config['lr'],
-                step_size_up=config['step_size_up'],
-                mode='exp_range',
-                gamma=0.999,
-                cycle_momentum=False
-            )
-        return optim, sched
-
 
     def step_temp(self):
         new_t = min(self.temp.item() * self.temp_anneal, self.max_temp)
